66c4bd84e6717e59b36afd0bcd3d660ae1055920
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-08-07 22:51:14 +0200

    Sonarqube

--- lint.sh
+++ lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-uv sync --frozen --group dev
+# Run `uv sync --frozen --group dev` first to set up/update the environment.
 uv run --frozen --no-build --no-sync ruff check custom_components/ --fix && \
 uv run --frozen --no-build --no-sync ruff format --check custom_components/ && \
 uv run --frozen --no-build --no-sync python -m pyright custom_components/
